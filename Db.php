751a96edd4456e219de42acf10865464f3e934b9
SuperCoalaMan <89955@glr.nl>
2023-04-20 12:37:03 +0200

    Create Db.php

---
+++ Db.php
@@ -0,0 +1,16 @@
+<?php
+//inloggegevens
+$serverIp = "web02.directnode.nl";
+$userName = "ukpqvruq_villareserveren";
+$passWord = "E5Szz00h8";
+$dataBaseName = "ukpqvruq_villareserveren";
+$serverPort = "3306";
+
+//Connect
+$connetion = new mysqli($serverIp, $userName, $passWord) or die ('Geen verbinding mogenlijk');
+mysqli_select_db($connetion, $dataBaseName) or die (mysqli_error($connetion));
+if ($connetion)
+{
+    echo("Conenctie voltoid");
+}
+
