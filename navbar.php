3d8de66d298cef8013d0dcb1bb8b6d273fd6c6fc
SuperCoalaMan <89955@glr.nl>
2023-05-20 12:04:53 +0200

    Navbar
    Laat maar weten wat jullie er van vinden.
    De icons moeten eig nog wit of zoiets maar dat regelen we nog wel

--- navbar.php
+++ navbar.php
@@ -1,3 +1,25 @@
+<link rel="stylesheet" href="navbar.css">
 <div class="Navbar">
-    <p>Yooooo</p>
+    <div class="navCompContainer">
+        <div class="container">
+            <div class="navComp">
+                <p><a href="#">Contact</a></p>
+            </div>
+            <div class="navComp">
+                <img class="icons" src="img/contactIcon.png">
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="navComp">
+                <p><a href="#">Login</a></p>
+            </div>
+            <div class="navComp">
+                <img class="icons" src="img/userIcon.png">
+            </div>
+        </div>
+        <div class="logo">
+            <img class="logoIcon" src="img/logovilla4u.png"
+        </div>
+    </div>
 </div>
