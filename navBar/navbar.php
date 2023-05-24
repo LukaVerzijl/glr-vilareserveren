19e52824a68c9c9a534ad177550128bf2111d227
Luka <redlucraft@gmail.com>
2023-05-24 14:17:58 +0200

    nav

--- navBar/navbar.php
+++ navBar/navbar.php
@@ -1,85 +1,125 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web 1920 – 1</title>
+    <style id="applicationStylesheet" type="text/css">
 
-<div class="Navbar">
-    <div class="navCompContainer">
-        <div class="container">
-            <div class="navComp">
-                <p><a href="#">Contact</a></p>
-            </div>
-            <div class="navComp">
-                <img class="icons" src="../img/contactIcon.png">
-            </div>
-        </div>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            border: none;
+        }
+        #Web_1920__1 {
+            position: absolute;
+            width: 1920px;
+            height: 1080px;
+            background-color: rgba(255,255,255,1);
+            overflow: hidden;
+            --web-view-name: Web 1920 – 1;
+            --web-view-id: Web_1920__1;
+            --web-scale-on-resize: true;
+            --web-enable-deep-linking: true;
+        }
+        #Rectangle_1 {
+            fill: rgba(26,34,45,1);
+        }
+        .Rectangle_1 {
+            position: absolute;
+            overflow: visible;
+            width: 1920px;
+            height: 142px;
+            left: 0px;
+            top: 0px;
+        }
+        #logovilla4u {
+            position: absolute;
+            width: 186px;
+            height: 110px;
+            left: 50px;
+            top: 19px;
+            overflow: visible;
+        }
+        #Rectangle_2 {
+            fill: rgba(17,75,95,1);
+        }
+        .Rectangle_2 {
+            position: absolute;
+            overflow: visible;
+            width: 149px;
+            height: 60px;
+            left: 1641px;
+            top: 42px;
+        }
+        #Sign-In {
+            left: 1657px;
+            top: 48px;
+            position: absolute;
+            overflow: visible;
+            width: 118px;
+            white-space: nowrap;
+            text-align: left;
+            font-family: Segoe UI;
+            font-style: normal;
+            font-weight: normal;
+            font-size: 37px;
+            color: rgba(255,255,255,1);
+        }
+        #Contact {
+            left: 1407px;
+            top: 47px;
+            position: absolute;
+            overflow: visible;
+            width: 128px;
+            white-space: nowrap;
+            text-align: left;
+            font-family: Segoe UI;
+            font-style: normal;
+            font-weight: normal;
+            font-size: 37px;
+            color: rgba(255,255,255,1);
+        }
+        #Home {
+            left: 1176px;
+            top: 47px;
+            position: absolute;
+            overflow: visible;
+            width: 100px;
+            white-space: nowrap;
+            text-align: left;
+            font-family: Segoe UI;
+            font-style: normal;
+            font-weight: normal;
+            font-size: 37px;
+            color: rgba(255,255,255,1);
+        }
+    </style>
 
-        <div class="container">
-            <div class="navComp">
-                <p><a href="#">R/L</a></p>
-            </div>
-            <div class="navComp">
-                <img class="icons" src="../img/userIcon.png">
-            </div>
-        </div>
+</head>
+<body>
+<div id="page">
+    <svg class="Rectangle_1">
+        <rect id="Rectangle_1" rx="0" ry="0" x="0" y="0" width="1920" height="142">
+        </rect>
+    </svg>
+    <img id="logovilla4u" src="logovilla4u.png" srcset="logovilla4u.png 1x, logovilla4u@2x.png 2x">
 
-        <div class="logo">
-            <img class="logoIcon" src="../img/logovilla4u.png">
-        </div>
+    <svg class="Rectangle_2">
+        <rect id="Rectangle_2" rx="10" ry="10" x="0" y="0" width="149" height="60">
+        </rect>
+    </svg>
+    <div id="Sign-In">
+        <span>Sign-In</span>
+    </div>
+    <div id="Contact">
+        <span>Contact</span>
+    </div>
+    <div id="Home">
+        <span>Home</span>
     </div>
 </div>
-<style>
-    /*Navbar container */
-
-.navbar{
-    height: 100px;
-    width: 100%;
-    background-color: #1a222d;
-    position: absolute;
-    -webkit-box-shadow: 1px 8px 8px 2px rgba(0,0,0,0.65);
-    box-shadow: 1px 8px 8px 2px rgba(0,0,0,0.65);
-}
-.navCompContainer{
-    margin-right: 30%;
-    height: auto;
-    width: auto;
-}
-/*Navbar component*/
-.navComp{
-    font-size: 20px;
-    float: right;
-    margin-right: 25px;
-    margin-top: 7%;
-}
-.container{
-    float: right;
-    transition: .2s;
-    height: 100px;
-    width: 200px;
-}
-.container:hover{
-    background-color: #2e4350;
-    -webkit-box-shadow: 0px 0px 17px 9px rgba(255,255,255,0.51);
-    box-shadow: 0px 0px 17px 9px rgba(255,255,255,0.51);
-}
-.icons{
-    height: 60px;
-    width: auto;
-    filter: invert(100%);
-}
-/*Navbar text*/
-.navComp a{
-    text-decoration: none;
-    color: white;
-}
-.logo{
-
-    margin-left: 35%;
-}
-.logoIcon{
-    -webkit-box-shadow: 0px 0px 17px 9px rgba(255,255,255,0.51);
-    box-shadow: 0px 0px 17px 9px rgba(255,255,255,0.51);
-    height: 100px;
-    width: 100px;
-}
-body{
-    margin: 0;
-    padding: 0;
-}
-</style>
+</body>
+</html>
