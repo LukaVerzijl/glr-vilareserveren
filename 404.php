1db83171facc58340109888768dec54a8a07263b
Luka Verzijl <redlucraft@gmail.com>
2023-05-26 09:03:29 +0200

    added 404

---
+++ 404.php
@@ -0,0 +1,82 @@
+ <style id="applicationStylesheet" type="text/css">
+
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            border: none;
+        }
+        #Web_1920__1 {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(255,255,255,1);
+            overflow: hidden;
+
+        }
+        #Rectangle_1 {
+            fill: rgba(55,74,103,1);
+        }
+        .Rectangle_1 {
+            position: absolute;
+            overflow: visible;
+            width: 100%;
+            height: 100%;
+            left: 0px;
+            top: 0px;
+        }
+        #Rectangle_2 {
+            fill: rgba(53,86,145,1);
+        }
+        .Rectangle_2 {
+            position: fixed;
+            left: 50%;
+            top: 80%;
+            transform: translate(-50%, -50%);
+            overflow: visible;
+            width: 335px;
+            height: 121px;
+
+        }
+        #Go_back {
+            position: fixed;
+            left: 50%;
+            top: 80%;
+            transform: translate(-50%, -50%);
+            overflow: visible;
+            width: 281px;
+            white-space: nowrap;
+            text-align: left;
+            font-family: Arial;
+            font-style: normal;
+            font-weight: bold;
+            font-size: 71px;
+            color: rgba(255,255,255,1);
+        }
+        #n_3 {
+            position: fixed;
+            left: 50%;
+            top: 35%;
+            transform: translate(-50%, -50%);
+            overflow: visible;
+        }
+    </style>
+</head>
+<body>
+<div id="Web_1920__1">
+    <svg class="Rectangle_1">
+        <rect id="Rectangle_1" rx="0" ry="0" x="0" y="0" width="1920" height="1080">
+        </rect>
+    </svg>
+    <svg class="Rectangle_2">
+        <rect id="Rectangle_2" rx="30" ry="30" x="0" y="0" width="335" height="121">
+        </rect>
+    </svg>
+    <div id="Go_back">
+        <span>Go back</span>
+    </div>
+    <img id="n_3" src="./img/404.png">
+
+</div>
+</body>
+</html>
