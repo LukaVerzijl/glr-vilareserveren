793a50ed6da3b45a5e460fdee2bbdd4b6f13aaac
Luka <redlucraft@gmail.com>
2023-05-30 11:58:36 +0200

    Complete villa pagina

--- biedingen.php
+++ biedingen.php
@@ -1,42 +1,30 @@
 <?php include 'db.php'; ?>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script>
-    function alert_succes() {
-        Swal.fire({
-            title: 'Succes!',
-            text: 'Uw bod is geplaatst',
-            icon: 'success',
-            confirmButtonText: 'Melding sluiten'
-        })
-    }
-    function alert_error() {
-        Swal.fire({
-            title: 'Error!',
-            text: 'Uw bod kon niet worden geplaatst omdat er dit bod al is geplaatst',
-            icon: 'error',
-            confirmButtonText: 'Melding sluiten'
-        })
-    }
-</script>
+
 <?php
 include 'db.php';
 $huis = '';
 $bodPrijs = 0;
 $gebruiker = '';
+$Achternaam = '';
+$Voornaam = '';
 $status = '';
 if(isset($_POST['submit'])) {
     $huis = ($_POST['huis']);
     $bodPrijs = ($_POST['bodPrijs']);
+    $Voornaam = ($_POST['Voornaam']);
+    $Achternaam = ($_POST['Achternaam']);
+
     $gebruiker = ($_POST['gebruiker']);
 
 
 
-    $query = "INSERT INTO `biedingen` (`Huis`, `BodPrijs`, `UserName`) VALUES ('" . $huis . "', '" . $bodPrijs . "', '" . $gebruiker . "')";
+    $query = "INSERT INTO `biedingen` (`Huis`, `BodPrijs`, `UserName`, `Voornaam`, `Achternaam`) VALUES ('" . $huis . "', '" . $bodPrijs . "', '" . $gebruiker . "','" . $Voornaam . "','" . $Achternaam . "')";
     try {
         mysqli_query($connetion, $query);
-        echo "<script> alert_succes(); </script>";
+        echo "<meta http-equiv='refresh' content='0'>";
+
     } catch (Exception $e) {
-        echo "<script> alert_error(); </script>";
     }
 }
 ?>
