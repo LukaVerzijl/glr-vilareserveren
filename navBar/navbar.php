a2426a9b1a8b23599f4dd56cb0e73708ace75451
Luka <redlucraft@gmail.com>
2023-05-31 14:33:24 +0200

    Update navbar.php

--- navBar/navbar.php
+++ navBar/navbar.php
@@ -1,21 +1,4 @@
-<link
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-        rel="stylesheet"
-/>
-<!-- Google Fonts -->
-<link
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        rel="stylesheet"
-/>
-<!-- MDB -->
-<link
-        href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.1/mdb.min.css"
-        rel="stylesheet"
-/>
-<script
-        type="text/javascript"
-        src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.1/mdb.min.js"
-></script>
+// Purpose: navbar for all pages
 
 <?php
 if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
