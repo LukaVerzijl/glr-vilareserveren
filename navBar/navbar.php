54f39704dfc1e0adf5736fb900f36116b99025a5
Luka Verzijl <redlucraft@gmail.com>
2023-06-05 12:21:42 +0200

    added favico

--- navBar/navbar.php
+++ navBar/navbar.php
@@ -24,6 +24,8 @@ $url.= $_SERVER['REQUEST_URI'];
 <head>
     <meta charset="utf-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <title>Villa4U</title>
+    <link rel="icon" type="image/png" href="../img/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style id="applicationStylesheet" type="text/css">
 
