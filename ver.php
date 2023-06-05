8925e1c8d973ca6a04290ca0e9b4c98477b989c5
SuperCoalaMan <89955@glr.nl>
2023-06-05 11:08:56 +0200

    ver system update

--- ver.php
+++ ver.php
@@ -9,10 +9,16 @@ $sql4 = "SELECT code FROM gebruikers WHERE code='$parCode'";
 $result2 = $connetion->query($sql4);
 $row2 = $result2->fetch_assoc();
 $checkCode = $row2['code'];
+echo $parCode;
+echo $checkCode;
 
-if ($checkCode === $parCode){
+$query = mysqli_query($connetion, "SELECT 'code' FROM 'gebruikers' WHERE 'code' = '$parCode'");
+if (empty($query)) {
+    $exists = "false";
+}
+else {
     $true = 1;
     $sql = "UPDATE gebruikers SET geactiveerd='$true' WHERE code='$parCode'";
-} else{
-    die;
+
+    mysqli_query($connetion, $sql);
 }
